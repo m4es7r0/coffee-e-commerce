d73c5bf2bab028801958e91b72de5af23961aefd
Dmytro <dmitrymusiichenko@gmail.com>
2023-01-21 10:36:58 +0200

    test commit from win

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -6,9 +6,7 @@ const Home: FC = () => {
   return (
     <Layout>
       <main className="hero flex h-screen w-full items-center justify-center">
-        <h1 className="text-center text-7xl font-bold text-green">
-          Hello world
-        </h1>
+        <h1 className="text-center text-7xl font-bold text-green">Coffee</h1>
       </main>
     </Layout>
   );
